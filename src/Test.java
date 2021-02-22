61710fee56916ec114ba4376e755038fcae8550d
Jacob Conrad <Jacob.Conrad@quinnipiac.edu>
2021-02-21 23:50:26 -0500

    Added the question does pineapple belong on pizza and takes a user input

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class Test {
-
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Does pineapple belong on pizza?");
+		String answer = input.nextLine();
+		if(answer.equals("yes"))
+		{
+			System.out.println("Disgusting.");
+		}
+		else if(answer.equals("no"))
+		{
+			System.out.println("That is the right answer.");
+		}
+		else System.out.println("You did not input yes or no");
+	}
 }
