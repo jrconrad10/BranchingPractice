7843d1e28cb107278690383bd674a794ff7c3022
Jacob Conrad <Jacob.Conrad@quinnipiac.edu>
2021-02-23 15:25:00 -0500

    Added an exitting message saying thank you for your input.

--- src/Test.java
+++ src/Test.java
@@ -37,5 +37,7 @@ public class Test {
 			else System.out.println("At least it's not pineapple.");
 		}
 		else System.out.println("You did not input yes or no");
+		
+		System.out.println("Thank you for your input!");
 	}
 }
