985e2cb57d61a309f7543ef33c5828948e6a72dd
Jacob Conrad <Jacob.Conrad@quinnipiac.edu>
2021-02-23 15:30:50 -0500

    Printed hello to the user to start the program.

--- src/Test.java
+++ src/Test.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Test {
 	public static void main(String[] args)
 	{
+		System.out.println("Hello!");
 		Scanner input = new Scanner(System.in);
 		System.out.println("Does pineapple belong on pizza?");
 		String answer = input.nextLine();
